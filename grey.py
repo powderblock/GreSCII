37470d7bce5f6458194a936854230635b1e5eb33
Blade Nelson <powderblock@gmail.com>
2015-07-14 18:35:59 -0400

    Show the image passed to grey.py
    In grey.py:
    -Take sys.argv[1] and show it to the user.

--- grey.py
+++ grey.py
@@ -1,1 +1,9 @@
-print "........................\n............."
+import cv2
+import numpy as np
+import sys
+
+img = cv2.imread(sys.argv[1], 0)
+
+cv2.imshow("img", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
