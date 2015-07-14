8187e3ec54d920e170ba623b968b8238c7360c1d
Blade Nelson <powderblock@gmail.com>
2015-07-14 19:15:11 -0400

    Show B, G, R for pixel at 100, 100
    In grey.py:
    -print the B, G, R values for the pixel at 100.
    -Remove greyscale loading.

--- grey.py
+++ grey.py
@@ -2,7 +2,11 @@ import cv2
 import numpy as np
 import sys
 
-img = cv2.imread(sys.argv[1], 0)
+img = cv2.imread(sys.argv[1])
+
+print "Red for pixel at 100, 100: " + str(img[100, 100, 2])
+print "Green for pixel at 100, 100: " + str(img[100, 100, 1])
+print "Blue for pixel at 100, 100: " + str(img[100, 100, 0])
 
 cv2.imshow("img", img)
 cv2.waitKey(0)
