fad9b9ba9a60477ec0b8b7cff521185d75a3578e
Blade Nelson <powderblock@gmail.com>
2015-07-15 01:23:15 -0400

    Convert to image greyscale, Add ASCII output
    In grey.py:
    -Add resizing so the image is easier to process
    -Open file ascii.txt
    -Create function turnIntoGrey()
    *Take red, green, and blue, return grey value. (This is calculated using
    the HDTV greyscale spec.)
    -Go through each pixel and use the turnIntoGrey() function to make that
    pixel grey. (Feed turnIntoGrey() the RGB values for each pixel.)
    -Append the ASCII art into the ascii list.
    -Write the new ASCII art to the file ascii.txt.

--- grey.py
+++ grey.py
@@ -3,10 +3,34 @@ import numpy as np
 import sys
 
 img = cv2.imread(sys.argv[1])
+img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 
-print "Red for pixel at 100, 100: " + str(img[100, 100, 2])
-print "Green for pixel at 100, 100: " + str(img[100, 100, 1])
-print "Blue for pixel at 100, 100: " + str(img[100, 100, 0])
+width = img.shape[0]
+height = img.shape[1]
+
+chars = [" ", ".", ":", "-", "=", "+", "*", "#", "%", "@"]
+ascii = []
+
+output = open("ascii.txt", "w+")
+
+# Function to take in a pixel's color values
+def turnIntoGrey(red, green, blue):
+    # Output a grey version of this pixel
+    return (0.2126 * red) + (.7152 * green) + (0.0722 * blue)
+
+# For every pixel in the image:
+for i in range(width):
+    for j in range(height):
+        red = img[i, j, 2], green = img[i, j, 1]
+        blue = img[i, j, 0]
+        grey = turnIntoGrey(red, green, blue)
+        img.itemset((i, j, 2), grey)
+        img.itemset((i, j, 1), grey)
+        img.itemset((i, j, 0), grey)
+        ascii.append(chars[int(round(grey, 0)) / 28])
+    ascii.append("\n")
+
+output.write("".join(ascii))
 
 cv2.imshow("img", img)
 cv2.waitKey(0)
